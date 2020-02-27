008a3c7e04b4408b2d816285c0e207b460e5fab4
Christan Spence <caspence@student.fullsail.edu>
2020-02-26 19:34:06 -0500

    yer

--- assests/code.js
+++ assests/code.js
@@ -4,22 +4,23 @@ let submit = $("#submit")
 let startTime = $("#start")
 let frequency = $("#frequency")
 
-        // Your web app's Firebase configuration
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAgcJRNmMnskExKtBZVvbct7yRdezqSmyQ",
-    authDomain: "trainscheduler-f3e50.firebaseapp.com",
-    databaseURL: "https://trainscheduler-f3e50.firebaseio.com",
-    projectId: "trainscheduler-f3e50",
-    storageBucket: "trainscheduler-f3e50.appspot.com",
-    messagingSenderId: "64165825466",
-    appId: "1:64165825466:web:71c521f51c90ad28932b83",
-    measurementId: "G-TZL7D8H5GK"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAgcJRNmMnskExKtBZVvbct7yRdezqSmyQ",
+  authDomain: "trainscheduler-f3e50.firebaseapp.com",
+  databaseURL: "https://trainscheduler-f3e50.firebaseio.com",
+  projectId: "trainscheduler-f3e50",
+  storageBucket: "trainscheduler-f3e50.appspot.com",
+  messagingSenderId: "64165825466",
+  appId: "1:64165825466:web:71c521f51c90ad28932b83",
+  measurementId: "G-TZL7D8H5GK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  let ref = database.ref();
+let database = firebase.database();
+let ref = database.ref();
 
 submit.on("click", function(e){
     e.preventDefault();
@@ -30,38 +31,55 @@ submit.on("click", function(e){
         frequency: frequency.val()
     })
     console.log(name.val(),)
-
     name.val("")
     destination.val("")
     startTime.val("")
     frequency.val("")
-
-
 })
 
-        // Firebase watcher + initial loader HINT: .on("value")
-        ref.on("child_added", function(snapshot) {
-            let table = $("#newrow");
-            let row = $("<tr>")
-            let data = $("<td>")
+// Firebase watcher + initial loader HINT: .on("value")
+ref.on("child_added", function(snapshot) {
+    let table = $("#newrow");
+    let row = $("<tr>")
+    let data = $("<td>")
 
-            let now = moment(new Date());
-            let start = moment(snapshot.val().start);
-            //let duration = moment.duration(now.diff(start));
-            //let months = Math.floor(duration.asMonths());
+    // Current Time
+    var currentTime = moment();
+    // Converting the time given from user input
+    var startTimeConverted = moment(snapshot.val().startTime, "HH:mm").subtract(1, "years");
+    console.log(startTimeConverted)
+    // Declaring the difference in time
+    var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+    console.log(moment())
+    console.log(startTimeConverted)
+    console.log(diffTime)
+    // The remanding time between now and the next train
+    var tRemainder = diffTime % snapshot.val().frequency;
+    console.log(tRemainder)
+    // Time in mins till next train
+    var tMinutesTillTrain = snapshot.val().frequency - tRemainder;
+    // The exact time till next train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
-            row.append(
-                $("<td>").text(snapshot.val().name),
-                $("<td>").text(snapshot.val().destination),
-                $("<td>").text(snapshot.val().start),
-                //$("<td>").text(months),
-                //$("<td>").text(`$${snapshot.val().rate}`),
-                //$("<td>").text(`$${months * snapshot.val().rate}`)
-                );
-            table.append(row);
+
+    //let duration = moment.duration(now.diff(start));
+    //let months = Math.floor(duration.asMonths());
+
+  row.append(
+    $("<td>").text(snapshot.val().name),
+    $("<td>").text(snapshot.val().destination),
+    $("<td>").text(snapshot.val().frequency),
+    $("<td>").text(moment(nextTrain).format("hh:mm")),
+    $("<td>").text(tMinutesTillTrain)
+    //$("<td>").text(months),
+    //$("<td>").text(`$${snapshot.val().rate}`),
+    //$("<td>").text(`$${months * snapshot.val().rate}`)
+);
+
+table.append(row);
       
-            // Handle the errors
-          }, function(errorObject) {
-            console.log("Errors handled: " + errorObject.code);
-          });
+// Handle the errors
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
 
